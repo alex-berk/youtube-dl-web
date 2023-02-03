b3b61c47bbf0628e19d4af7432f9a330de52c074
Alex Berk <aleks.berkov@gmail.com>
2023-02-03 18:42:21 +0200

    connect controller to the app

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from controller import download
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    video_url = request.args.get("url")
+    if video_url:
+        download(video_url)
     return render_template("index.html")
 
 
